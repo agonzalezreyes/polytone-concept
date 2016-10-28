ef4727a638dafa74009413d9e51983617e43c6e4
Alejandrina Gonzalez Reyes <agonzalezreyes@outlook.com>
2016-10-28 12:38:41 -0700

    Initial Commit

---
+++ Polytone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Polytone
+//
+//  Created by Alejandrina Gonzalez on 10/28/16.
+//  Copyright © 2016 Alejandrina Gonzalez Reyes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
